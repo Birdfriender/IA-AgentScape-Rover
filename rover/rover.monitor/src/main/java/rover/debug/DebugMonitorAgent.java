39e3d39aebfd1855038377da4ec58ec7385d3464
Birdfriender <idolwitchviolet@gmail.com>
2016-11-24 12:43:35 +0000

    Added debug monitor actually to git

---
+++ rover/rover.monitor/src/main/java/rover/debug/DebugMonitorAgent.java
@@ -0,0 +1,131 @@
+package rover.debug;
+
+import javax.swing.SwingUtilities;
+
+import org.iids.aos.agent.Agent;
+import org.iids.aos.service.ServiceBroker;
+
+import org.iids.aos.service.ServiceException;
+import rover.DebugRoverDisplay;
+import rover.IRoverService;
+import rover.MonitorInfo;
+import rover.MonitorInfo.Resource;
+import rover.MonitorInfo.Rover;
+import rover.MonitorInfo.Team;
+
+
+public class DebugMonitorAgent extends Agent  {
+
+	private static final long serialVersionUID = 1L;
+
+	private IRoverService service;
+
+	private ServiceBroker sb;
+	private DebugRoverDisplay display;
+
+	public DebugMonitorAgent() {
+		service = null;
+		sb = null;
+		display = null;
+
+		//preload cause of agentscape bug;
+		MonitorInfo mi = new MonitorInfo(0, 0);
+		Rover r = mi.new Rover(0, 0, "");
+		Team t = mi.new Team(0, 0, "", 0);
+		Resource rs = mi.new Resource(0, 0, 0, 1);
+
+	}
+
+	@Override
+	public void run() {
+
+		sb = getServiceBroker();
+
+
+
+		//Runnable createDisplay = new Runnable() {
+		//  @Override
+		//  public void run() {
+
+		display = new DebugRoverDisplay(sb);
+
+		display.setVisible(true);
+		//}
+		//};
+
+		//SwingUtilities.invokeLater(createDisplay);
+
+		runMonitor();
+
+
+	}
+
+
+
+
+	private void runMonitor() {
+
+		boolean connected = true;
+		Runnable updateDisplay = new Runnable() {
+			@Override
+			public void run() {
+				display.UpdateDisplay(service.getWorldInfo());
+
+			}
+		};
+
+		while(display.isVisible() &&  connected) {
+
+			try {
+				service = sb.bind(IRoverService.class);
+				connected = true;
+
+			} catch (ServiceException s) {
+				connected = false;
+				return;
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+
+				return;
+			}
+
+			SwingUtilities.invokeLater(updateDisplay);
+					
+					/*
+					
+					boolean found = false;
+					
+					for(Rover r : m.getRovers()) {
+						Log.console("[monitor] Rover " + r.getKey() + " x:" + r.getX() + " y:" + r.getY() + " energy:" + r.getEnergy() + " load:" + r.getCurrentLoad());
+						if(r.getEnergy() > 0) {
+							found = true;
+						}
+					}
+					
+					if(!found) {
+						//all rovers are out of energy
+						Log.console("[monitor] All rovers dead.");
+						
+						for(Team t  : m.getTeams()) {
+							Log.console("[monitor] Team " + t.getTeamName() + " collected: " + t.getTotalCollected());
+						}
+						
+						break;
+					}
+					*/
+
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+
+	}
+
+
+}
